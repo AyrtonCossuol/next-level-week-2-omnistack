69c6ebe571fde115fc49ae630d3c7658278e8e1b
AyrtonCossuol <ayrton.cossuol@live.com>
2020-08-04 10:03:16 -0300

    starting the backend

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    
+    const users = [
+        { name: 'Ayrton', agr: 25 },
+    ];
+    
+    return response.json(users);
+});
+
+app.listen(3333);
