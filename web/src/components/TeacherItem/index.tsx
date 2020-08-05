00857dd0b8fd03244fddc529756fce00437f9316
AyrtonCossuol <ayrton.cossuol@live.com>
2020-08-05 16:14:43 -0300

    listing page completed and next to the backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,37 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnections() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/43251314?s=460&u=128036808cc94fbc8336199d58f2c0fe3f9584a9&v=4" alt="Ayrton Cossuol"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
 
                 <div>
-                    <strong>Ayrton Cossuol</strong>
-                    <span>Gagueira</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Gaguejando mais que tudo
-
-                <br/> <br/>
-
-                Venha gagueijar voce tambem
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type='button'>
+                <a 
+                    onClick={createNewConnections} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    target='_blank'
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
