2c75a498a5c218aac5b3f0ed8013c4dbfae95d0e
AyrtonCossuol <ayrton.cossuol@live.com>
2020-08-07 14:47:33 -0300

    list of teachers and favorites finalized

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+function TeacherItem() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image 
+                    style={styles.avatar}
+                    source={{ uri: 'https://github.com/AyrtonCossuol.png' }}
+                />
+
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>Ayrton Cossuol</Text>
+                    <Text style={styles.subject}>Gagueira</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                LAalalalalalalalalalalalalallalalala
+                {'\n'}
+                LAalalalalalalalalalalalalallalalalaLAalalalalalalalalalalalalallalalalaLAalalalalalalalalalalalalallalalalaLAalalalalalalalalalalalalallalalalaLAalalalalalalalalalalalalallalalala
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'  '}
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        {/* <Image source={heartOutlineIcon} /> */}
+                        <Image source={unFavoriteIcon} />
+                    </RectButton>
+                    
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entra em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+        </View>
+    );
+}   
+
+export default TeacherItem;
